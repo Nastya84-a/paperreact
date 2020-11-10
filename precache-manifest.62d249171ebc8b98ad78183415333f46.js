b4bda9b2228116d0cbd4c1bcb356a76c9f803923
Anastasya Kushnir <anastasyakyshnir@gmail.com>
2020-11-10 09:42:02 +0200

    Updates

---
+++ precache-manifest.62d249171ebc8b98ad78183415333f46.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f071b83ecc495f6025abb2965906c42b",
+    "url": "/paperreact/index.html"
+  },
+  {
+    "revision": "5be12158309a466662d4",
+    "url": "/paperreact/static/css/2.bbefbc3e.chunk.css"
+  },
+  {
+    "revision": "4601222c06b4bf46faba",
+    "url": "/paperreact/static/css/main.5433c6b9.chunk.css"
+  },
+  {
+    "revision": "5be12158309a466662d4",
+    "url": "/paperreact/static/js/2.74d519fd.chunk.js"
+  },
+  {
+    "revision": "74576634b1ad091cd02d1c3a3bb53d12",
+    "url": "/paperreact/static/js/2.74d519fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4601222c06b4bf46faba",
+    "url": "/paperreact/static/js/main.9531a324.chunk.js"
+  },
+  {
+    "revision": "ac24c3862396b04df798",
+    "url": "/paperreact/static/js/runtime-main.45f25937.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/paperreact/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/paperreact/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/paperreact/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/paperreact/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/paperreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/paperreact/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/paperreact/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/paperreact/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/paperreact/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/paperreact/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
